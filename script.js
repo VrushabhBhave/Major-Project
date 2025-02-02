a821ffb7536918c908438e1f11014af1119954cf
Vrushabhbhave <vrushabhbhave31@gmail.com>
2025-02-02 17:19:07 +0530

    add swiper section

---
+++ script.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+      850: {
+        slidesPerView: 3,
+      },
+
+      500: {
+        slidesPerView: 2,
+      },
+
+      300: {
+        slidesPerView: 1,
+      },
+    },
+  });
